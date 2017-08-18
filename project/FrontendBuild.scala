0c6b3f99b9607ab87306afdddf24bdf3bca30432
Miranda Hawkes <miranda.hawkes@digital.hmrc.gov.uk>
2017-08-18 11:57:33 +0100

    Added mockito, scalastyle and scoverage

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -28,7 +28,8 @@ object FrontendBuild extends Build with MicroService {
     "org.scalatest" %% "scalatest" % "3.0.0" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "org.jsoup" % "jsoup" % "1.10.2" % scope,
-    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
+    "org.mockito" % "mockito-core" % "2.8.47" % scope
   )
 
 }
