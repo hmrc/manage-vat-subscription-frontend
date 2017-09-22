342bab33da442e12614146927934759def088246
Thomas Dudley <thomas.dudley@digital.hmrc.gov.uk>
2017-09-22 16:23:20 +0100

    Added Auth predicates

---
+++ app/auth/AuthPredicates.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIED OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package auth
+
+import cats.implicits._
+import auth.AuthPredicate.{AuthPredicate, Success}
+import play.api.mvc.{Result, Results}
+import uk.gov.hmrc.http.SessionKeys.{authToken, lastRequestTimestamp}
+
+import scala.concurrent.Future
+
+object AuthPredicates extends Results {
+
+  lazy val notEnrolled: Result = Redirect(controllers.routes.HelloWorldController.helloWorld())
+  lazy val timeRoute: Result = Redirect(controllers.routes.HelloWorldController.helloWorld())
+
+  private val enrolledPredicate: AuthPredicate = request => user =>
+    if (user.mtdVatID.nonEmpty) {
+      Right(Success)
+    } else {
+      Left(Future.successful(notEnrolled))
+    }
+
+  private val timeoutPredicate: AuthPredicate = request => user =>
+    if (request.session.get(lastRequestTimestamp).nonEmpty && request.session.get(authToken).isEmpty) {
+      Left(Future.successful(timeRoute))
+    }
+    else {
+      Right(Success)
+    }
+
+  val enrolledPredicates: AuthPredicate = enrolledPredicate |+| timeoutPredicate
+
+
+}
