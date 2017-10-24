d507cb85b19d6919e86be1b9cdc25f4ffa86d577
mac.grewal <mac.grewal@digital.hmrc.gov.uk>
2017-10-24 09:38:07 +0100

    Added stacked option to radio helper

--- test/views/templates/inputs/RadioGroupTemplateSpec.scala
+++ test/views/templates/inputs/RadioGroupTemplateSpec.scala
@@ -42,53 +42,42 @@ class RadioGroupTemplateSpec extends TemplateBaseSpec {
        |    <input type="radio" id="$fieldName-$value" name="$fieldName" value="$value"${if (checked) " checked" else ""}>
        |    <label for="$fieldName-$value"> $display </label>
        |  </div>
-       |
       """.stripMargin
 
   "Calling the radio group helper with an choice pre-selected" should {
 
-    "render a list of radio options with one pre-selected" in {
+    "render a list of radio options with one pre-checked" in {
       val field: Field = Field(TextInputForm.form, fieldName, Seq(), None, Seq(), Some("value2"))
       val expectedMarkup = Html(
         s"""
-           |
            |  <div class="form-group">
            |    <fieldset class="inline">
-           |
            |      <legend>
            |        <h1 class="heading-medium">
            |          $labelText
            |        </h1>
            |      </legend>
-           |
            |      ${generateExpectedRadioMarkup("value1", "display1")}
            |      ${generateExpectedRadioMarkup("value2", "display2", true)}
            |      ${generateExpectedRadioMarkup("value3", "display3")}
            |      ${generateExpectedRadioMarkup("value4", "display4")}
            |      ${generateExpectedRadioMarkup("value5", "display5")}
-           |
            |    </fieldset>
            |  </div>
-           |
         """.stripMargin
       )
 
       val markup = radioGroup(field, choices, labelText)
-
       formatHtml(markup) shouldBe formatHtml(expectedMarkup)
-
     }
   }
-
 
   "Calling the radio helper with a list of available choices" should {
 
     "render the choices as radio buttons" in {
-
       val field: Field = Field(TextInputForm.form, fieldName, Seq(), None, Seq(), None)
       val expectedMarkup = Html(
         s"""
-           |
            |  <div class="form-group">
            |    <fieldset class="inline">
            |
@@ -106,14 +95,43 @@ class RadioGroupTemplateSpec extends TemplateBaseSpec {
            |
            |    </fieldset>
            |  </div>
-           |
         """.stripMargin
       )
 
       val markup = radioGroup(field, choices, labelText)
-
       formatHtml(markup) shouldBe formatHtml(expectedMarkup)
-
     }
   }
+
+  "Calling the radio helper with the stacked option enabled" should {
+
+    "render the choices as radio buttons in a stacked layout" in {
+      val field: Field = Field(TextInputForm.form, fieldName, Seq(), None, Seq(), None)
+      val expectedMarkup = Html(
+        s"""
+           |  <div class="form-group">
+           |    <fieldset class="">
+           |
+           |      <legend>
+           |        <h1 class="heading-medium">
+           |          $labelText
+           |        </h1>
+           |      </legend>
+           |
+           |      ${generateExpectedRadioMarkup("value1", "display1")}
+           |      ${generateExpectedRadioMarkup("value2", "display2")}
+           |      ${generateExpectedRadioMarkup("value3", "display3")}
+           |      ${generateExpectedRadioMarkup("value4", "display4")}
+           |      ${generateExpectedRadioMarkup("value5", "display5")}
+           |
+           |    </fieldset>
+           |  </div>
+        """.stripMargin
+      )
+
+      val markup = radioGroup(field, choices, labelText, stacked = true)
+      formatHtml(markup) shouldBe formatHtml(expectedMarkup)
+    }
+  }
+
 }
