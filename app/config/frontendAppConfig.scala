064bb2aad6e9588b2b0e672b0953ec1b4063f3fc
Miranda Hawkes <miranda.hawkes@digital.hmrc.gov.uk>
2017-11-01 11:41:44 +0000

    Renamed ggServiceUrl to signInUrl

--- app/config/frontendAppConfig.scala
+++ app/config/frontendAppConfig.scala
@@ -32,7 +32,7 @@ trait AppConfig extends ServicesConfig {
   val whitelistedIps: Seq[String]
   val whitelistExcludedPaths: Seq[Call]
   val shutterPage: String
-  val ggServiceUrl: String
+  val signInUrl: String
   val authUrl: String
 }
 
@@ -65,6 +65,6 @@ class FrontendAppConfig @Inject()(val app: Application) extends AppConfig {
   private lazy val signInContinueBaseUrl: String = configuration.getString("signIn.continueBaseUrl").getOrElse("")
   private lazy val signInContinueUrl: String = ContinueUrl(signInContinueBaseUrl + controllers.routes.HelloWorldController.helloWorld().url).encodedUrl
   private lazy val signInOrigin = loadConfig("appName")
-  override lazy val ggServiceUrl: String = s"$signInBaseUrl?continue=$signInContinueUrl&origin=$signInOrigin"
+  override lazy val signInUrl: String = s"$signInBaseUrl?continue=$signInContinueUrl&origin=$signInOrigin"
 
 }
