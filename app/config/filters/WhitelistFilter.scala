d0ea0750412628cad1e7c69eecf42ee8649b1869
mac.grewal <mac.grewal@digital.hmrc.gov.uk>
2018-01-02 10:59:40 +0000

    Updated copyright statement to reflect new year

--- app/config/filters/WhitelistFilter.scala
+++ app/config/filters/WhitelistFilter.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,4 +44,3 @@ class WhitelistFilter @Inject()(val appConfig: AppConfig, implicit val mat: Mate
   }
 
 }
-
