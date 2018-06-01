eabef45e2f9b9860d022cd75ad21e863bb0eb650
Ian Seddon <ian.seddon@digital.hmrc.gov.uk>
2018-06-01 16:39:07 +0100

    BTAT-2753 more comprehensive unit test for change of circumstances unmarshalling

--- test/models/circumstanceInfo/CircumstanceDetailsModelSpec.scala
+++ test/models/circumstanceInfo/CircumstanceDetailsModelSpec.scala
@@ -26,34 +26,138 @@ class CircumstanceDetailsModelSpec extends UnitSpec {
 
     "succeeds when all registration fields are populated" in {
       val unmarshalled = Json.parse(CircumstanceDetailsTestConstants.fullPopulation).as[CircumstanceDetails]
-      assert(unmarshalled.bankDetails.get.sortCode == Some("77****"))
+      assert(unmarshalled.bankDetails.get.sortCode.contains("77****"))
+      assert(unmarshalled.bankDetails.get.accountHolderName.contains("***********************"))
+      assert(unmarshalled.bankDetails.get.bankAccountNumber.contains("****5274"))
     }
 
     "succeeds when optional banking details are not supplied" in {
       val unmarshalled = Json.parse(CircumstanceDetailsTestConstants.noBanking).as[CircumstanceDetails]
+
+      // Only the bank details should be un-populated
       assert(unmarshalled.bankDetails.isEmpty)
+
+      // The rest of the object should be populated
+      assert(unmarshalled.businessName.contains("Ancient Antiques"))
+
+      assert(unmarshalled.flatRateScheme.get.FRSCategory.contains("001"))
+      assert(unmarshalled.flatRateScheme.get.FRSPercentage.contains(123.12))
+      assert(unmarshalled.flatRateScheme.get.limitedCostTrader.contains(true))
+      assert(unmarshalled.flatRateScheme.get.startDate.contains("2001-01-01"))
+
+      assert(unmarshalled.ppob.get.address.get.line1.contains("VAT ADDR 1"))
+      assert(unmarshalled.ppob.get.address.get.line2.contains("VAT ADDR 2"))
+      assert(unmarshalled.ppob.get.address.get.line3.contains("VAT ADDR 3"))
+      assert(unmarshalled.ppob.get.address.get.line4.contains("VAT ADDR 4"))
+      assert(unmarshalled.ppob.get.address.get.line5.isEmpty)
+      assert(unmarshalled.ppob.get.address.get.postCode.contains("SW1A 2BQ"))
+      assert(unmarshalled.ppob.get.address.get.countryCode.contains("ES"))
+
+      assert(unmarshalled.returnPeriod.get.stdReturnPeriod.contains("MM"))
+
     }
 
     "succeeds when optional business name is not supplied" in {
       val unmarshalled = Json.parse(CircumstanceDetailsTestConstants.noBusinessName).as[CircumstanceDetails]
+
+      // Only the business name should be un-populated
       assert(unmarshalled.businessName.isEmpty)
+
+      // The rest of the object should be populated
+      assert(unmarshalled.bankDetails.get.sortCode.contains("77****"))
+      assert(unmarshalled.bankDetails.get.accountHolderName.contains("***********************"))
+      assert(unmarshalled.bankDetails.get.bankAccountNumber.contains("****5274"))
+
+      assert(unmarshalled.flatRateScheme.get.FRSCategory.contains("001"))
+      assert(unmarshalled.flatRateScheme.get.FRSPercentage.contains(123.12))
+      assert(unmarshalled.flatRateScheme.get.limitedCostTrader.contains(true))
+      assert(unmarshalled.flatRateScheme.get.startDate.contains("2001-01-01"))
+
+      assert(unmarshalled.ppob.get.address.get.line1.contains("VAT ADDR 1"))
+      assert(unmarshalled.ppob.get.address.get.line2.contains("VAT ADDR 2"))
+      assert(unmarshalled.ppob.get.address.get.line3.contains("VAT ADDR 3"))
+      assert(unmarshalled.ppob.get.address.get.line4.contains("VAT ADDR 4"))
+      assert(unmarshalled.ppob.get.address.get.line5.isEmpty)
+      assert(unmarshalled.ppob.get.address.get.postCode.contains("SW1A 2BQ"))
+      assert(unmarshalled.ppob.get.address.get.countryCode.contains("ES"))
+
+      assert(unmarshalled.returnPeriod.get.stdReturnPeriod.contains("MM"))
     }
 
     "succeeds when optional Flat Rate Scheme values are not supplied" in {
       val unmarshalled = Json.parse(CircumstanceDetailsTestConstants.noFlatRateScheme).as[CircumstanceDetails]
       assert(unmarshalled.businessName == Some("Ancient Antiques"))
+
+      // Only the flat rate scheme should be un-populated
       assert(unmarshalled.flatRateScheme.isEmpty)
+
+      // The rest of the object should be populated
+      assert(unmarshalled.businessName.contains("Ancient Antiques"))
+
+      assert(unmarshalled.bankDetails.get.sortCode.contains("77****"))
+      assert(unmarshalled.bankDetails.get.accountHolderName.contains("***********************"))
+      assert(unmarshalled.bankDetails.get.bankAccountNumber.contains("****5274"))
+
+      assert(unmarshalled.ppob.get.address.get.line1.contains("VAT ADDR 1"))
+      assert(unmarshalled.ppob.get.address.get.line2.contains("VAT ADDR 2"))
+      assert(unmarshalled.ppob.get.address.get.line3.contains("VAT ADDR 3"))
+      assert(unmarshalled.ppob.get.address.get.line4.contains("VAT ADDR 4"))
+      assert(unmarshalled.ppob.get.address.get.line5.isEmpty)
+      assert(unmarshalled.ppob.get.address.get.postCode.contains("SW1A 2BQ"))
+      assert(unmarshalled.ppob.get.address.get.countryCode.contains("ES"))
+
+      assert(unmarshalled.returnPeriod.get.stdReturnPeriod.contains("MM"))
+
+
     }
 
     "succeeds when optional PPOB values are not supplied" in {
       val unmarshalled = Json.parse(CircumstanceDetailsTestConstants.noPPOB).as[CircumstanceDetails]
-      assert(unmarshalled.businessName == Some("Ancient Antiques"))
+
+      // Only the ppop data should not be populated
       assert(unmarshalled.ppob.isEmpty)
+
+      // The rest of the object should be populated
+      assert(unmarshalled.businessName == Some("Ancient Antiques"))
+
+      assert(unmarshalled.bankDetails.get.sortCode.contains("77****"))
+      assert(unmarshalled.bankDetails.get.accountHolderName.contains("***********************"))
+      assert(unmarshalled.bankDetails.get.bankAccountNumber.contains("****5274"))
+
+      assert(unmarshalled.flatRateScheme.get.FRSCategory.contains("001"))
+      assert(unmarshalled.flatRateScheme.get.FRSPercentage.contains(123.12))
+      assert(unmarshalled.flatRateScheme.get.limitedCostTrader.contains(true))
+      assert(unmarshalled.flatRateScheme.get.startDate.contains("2001-01-01"))
+
+      assert(unmarshalled.returnPeriod.get.stdReturnPeriod.contains("MM"))
     }
 
     "succeeds when optional Return Period values are not supplied" in {
       val unmarshalled = Json.parse(CircumstanceDetailsTestConstants.noReturnPeriod).as[CircumstanceDetails]
+
+      // Only the return period should not be populated
       assert(unmarshalled.returnPeriod.isEmpty)
+
+      // The rest of the object should be populated
+      assert(unmarshalled.businessName.contains("Ancient Antiques"))
+
+      assert(unmarshalled.bankDetails.get.sortCode.contains("77****"))
+      assert(unmarshalled.bankDetails.get.accountHolderName.contains("***********************"))
+      assert(unmarshalled.bankDetails.get.bankAccountNumber.contains("****5274"))
+
+      assert(unmarshalled.flatRateScheme.get.FRSCategory.contains("001"))
+      assert(unmarshalled.flatRateScheme.get.FRSPercentage.contains(123.12))
+      assert(unmarshalled.flatRateScheme.get.limitedCostTrader.contains(true))
+      assert(unmarshalled.flatRateScheme.get.startDate.contains("2001-01-01"))
+
+      assert(unmarshalled.ppob.get.address.get.line1.contains("VAT ADDR 1"))
+      assert(unmarshalled.ppob.get.address.get.line2.contains("VAT ADDR 2"))
+      assert(unmarshalled.ppob.get.address.get.line3.contains("VAT ADDR 3"))
+      assert(unmarshalled.ppob.get.address.get.line4.contains("VAT ADDR 4"))
+      assert(unmarshalled.ppob.get.address.get.line5.isEmpty)
+      assert(unmarshalled.ppob.get.address.get.postCode.contains("SW1A 2BQ"))
+      assert(unmarshalled.ppob.get.address.get.countryCode.contains("ES"))
+
     }
 
   }
