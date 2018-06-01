5ae2b44abaae167d7e886282dd5faddfff446b9c
samuel-raspin <samuel.raspin@digital.hmrc.gov.uk>
2018-06-01 12:11:50 +0100

    [BTAT-2757] - WIP: paymentsService

---
+++ test/services/PaymentsServiceSpec.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import assets.BaseTestConstants.errorModel
+import mocks.connectors.MockPaymentsConnector
+import models.core.ErrorModel
+import models.payments.PaymentRedirectModel
+import utils.TestUtil
+
+import scala.concurrent.Future
+
+class PaymentsServiceSpec extends TestUtil with MockPaymentsConnector {
+
+  object TestPaymentsService extends PaymentsService(mockPaymentsConnector)
+
+  "PaymentsService" should {
+
+    def result: Future[Either[ErrorModel, PaymentRedirectModel]] = TestPaymentsService.postPaymentDetails(vrn)
+
+    "for getCustomerDetails method" when {
+
+      "called for a Right with CustomerDetails" should {
+
+        "return a CustomerDetailsModel" in {
+          setupMockPostPaymentsDetails(vrn)(Right(individual))
+          await(result) shouldBe Right(individual)
+        }
+      }
+
+      "given an error should" should {
+
+        "return an Left with an ErrorModel" in {
+          setupMockUserDetails(vrn)(Left(errorModel))
+          await(result) shouldBe Left(errorModel)
+        }
+      }
+    }
+  }
+
+}
