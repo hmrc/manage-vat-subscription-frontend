92076b73a9b1c72d62595209d05894c59eacfe06
Miranda Hawkes <miranda.hawkes@digital.hmrc.gov.uk>
2018-06-06 16:20:08 +0100

    Updated stub config

--- app/testOnly/TestOnlyAppConfig.scala
+++ app/testOnly/TestOnlyAppConfig.scala
@@ -24,6 +24,6 @@ class TestOnlyAppConfig @Inject()(env: Environment,
                                   config: Configuration)
   extends FrontendAppConfig(config, env){
 
-  lazy val dynamicStubUrl: String = baseUrl("vat-subscription")
+  lazy val dynamicStubUrl: String = baseUrl("manage-vat-subscription-dynamic-stub")
 
 }
