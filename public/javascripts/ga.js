0b9e5cc34f694eefd61289c1c6e80fa4d72bfa48
samuel-raspin <samuel.raspin@digital.hmrc.gov.uk>
2018-07-11 10:47:59 +0100

    [BTAT-3248] - GA corrections and tweaks

--- public/javascripts/ga.js
+++ public/javascripts/ga.js
@@ -1,22 +1,18 @@
+function sendGAEvent(category, action, label, isAgent) {
+    if (isAgent || isAgent == "true") {
+        ga('send', 'event', 'agent_' + category, action, label);
+    } else {
+        ga('send', 'event', category, action, label);
+    }
+}
+
 $(document).ready($(function () {
 
     $('[data-metrics]').each(function () {
         var metrics = $(this).attr('data-metrics');
         var splitOnUser = metrics.split(';');
         var parts = splitOnUser[0].split(':');
-        if(splitOnUser[1] == "true"){
-            ga('send', 'event', 'agent_' + parts[0], parts[1], parts[2]);
-        } else {
-            ga('send', 'event', parts[0], parts[1], parts[2]);
-        }
+        sendGAEvent(parts[0], parts[1], parts[2], splitOnUser[1]);
     });
 
 }));
-
-function clickToGA(category, action, label, isAgent) {
-    if (isAgent) {
-        ga('send', 'event', 'agent_' + category, action, label);
-    } else {
-        ga('send', 'event', category, action, label);
-    }
-}
