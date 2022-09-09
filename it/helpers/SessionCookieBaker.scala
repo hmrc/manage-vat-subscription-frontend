8c8aa7f0a771b086ac67e935de925168cfe9dfdb
william-hatzar <22661741+william-hatzar@users.noreply.github.com>
2022-09-09 10:18:19 +0100

    WIP

--- it/helpers/SessionCookieBaker.scala
+++ it/helpers/SessionCookieBaker.scala
@@ -20,7 +20,7 @@ import java.net.URLEncoder
 
 import play.api.http.SecretConfiguration
 import play.api.libs.crypto.DefaultCookieSigner
-import uk.gov.hmrc.crypto.{CompositeSymmetricCrypto, PlainText}
+import uk.gov.hmrc.crypto.{SymmetricCryptoFactory, PlainText}
 
 object SessionCookieBaker {
 
@@ -37,7 +37,7 @@ object SessionCookieBaker {
     }
 
     val encodedCookie = encode(sessionData)
-    val encrypted = CompositeSymmetricCrypto.aesGCM(cookieKey, Seq()).encrypt(encodedCookie).value
+    val encrypted = SymmetricCryptoFactory.aesCrypto(cookieKey).encrypt(encodedCookie).value
 
     s"""mdtp="$encrypted"; Path=/; HTTPOnly"; Path=/; HTTPOnly"""
   }
